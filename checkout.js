b0297c765fd9bce71ab56fe3a24090b0aeb01732
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-06-02 21:17:32 +0300

    update

--- checkout.js
+++ checkout.js
@@ -1,1 +1,24 @@
+const taxRate = 0.18;
+const shippingPrice = 15.0;
+
+window.addEventListener("load", ()=>{
+    //!set item to LocalStorage
+    localStorage.setItem("taxRate", taxRate);
+    localStorage.setItem("shippingPrice", shippingPrice);
+
+    //!set item to SessionStorage
+    // sessionStorage.setItem("taxRate", taxRate);
+    // sessionStorage.setItem("shippingPrice", shippingPrice);
+});
+
+//*****Capturing method*****//
+
+let productsDiv = document.querySelector(".products");
+productsDiv.addEventListener("click", (event)=>{
+    console.log(event.target);
+
+});
+
+
+
 
