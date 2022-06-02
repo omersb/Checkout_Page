500aa07cc0b2689b9724ba9dd876838154b70af3
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-06-02 21:35:39 +0300

    update + , -, remove

--- checkout.js
+++ checkout.js
@@ -6,19 +6,37 @@ window.addEventListener("load", ()=>{
     localStorage.setItem("taxRate", taxRate);
     localStorage.setItem("shippingPrice", shippingPrice);
 
-    //!set item to SessionStorage
-    // sessionStorage.setItem("taxRate", taxRate);
-    // sessionStorage.setItem("shippingPrice", shippingPrice);
+     //!set item to SessionStorage
+    //  sessionStorage.setItem("taxRate", taxRate);
+    //  sessionStorage.setItem("shippingPrice", shippingPrice);
+
 });
 
-//*****Capturing method*****//
-
+//*****capturing method*****//
 let productsDiv = document.querySelector(".products");
 productsDiv.addEventListener("click", (event)=>{
-    console.log(event.target);
-
+    if (event.target.className == "minus") {
+        let quantityp = event.target.nextElementSibling;
+        if (quantityp.innerText > 1) {
+            quantityp.innerText--;
+        }
+        else{
+            if (confirm("Product will be deleted?")) {
+                event.target.parentElement.parentElement.parentElement.remove();
+            }
+        }
+        // console.log(typeof event.target.nextElementSibling.innerText);
+        // console.log("minus button clicked");
+    }
+    else if(event.target.classList.contains("plus")){
+        event.target.previousElementSibling.innerText++;
+        // console.log("plus button clicked");
+    }
+    else if(event.target.classList.contains("remove-product")){
+        event.target.parentElement.parentElement.parentElement.remove();
+        // console.log("remove button clicked");
+    }
+    else{
+        // console.log("other element clicked");
+    }
 });
-
-
-
-
